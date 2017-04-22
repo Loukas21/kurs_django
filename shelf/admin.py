f8795edfcc5329a457d9011f266d98dbb26a3487
Loukas21 <lukaszgrzegorzskibinski@gmail.com>
2017-04-22 18:11:38 +0200

    kolejny commit

--- shelf/admin.py
+++ shelf/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Author, Publisher, Book
+from .models import Author, Publisher, Book # . oznacza,że to w tej aplikacji
 
+class AuthorAdmin(admin.ModelAdmin):#konwencja, AuthorAdmin dziedziczy po admin.ModelAdmin
+	search_fields = ['last_name', 'first_name']
+	ordering = ['last_name', 'first_name']
+	
+class BookAdmin(admin.ModelAdmin):
+	search_fields = ['title']
+	list_display = ['title', 'author', 'isbn', 'publisher']
+	
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin) # w nawiasie model, klasa administrująca
 # Register your models here
-admin.site.register([Author, Publisher, Book])
+admin.site.register(Publisher)
 
--- shelf/models.py
+++ shelf/models.py
@@ -4,14 +4,21 @@ from django.db import models
 class Author(models.Model):
 	first_name = models.CharField(max_length=20)
 	last_name = models.CharField(max_length=50)
-	
+	def __str__(self):
+		return "{first_name} {last_name}".format(first_name=self.first_name,last_name=self.last_name)
 	
 class Publisher(models.Model):
 	name = models.CharField(max_length=70)
-
+	def __str__(self):
+		return self.name
+	
 
 class Book(models.Model):
 	title = models.CharField(max_length=100)
 	author = models.ForeignKey(Author) # jezeli klasa Author bylaby po klasie Book, to mozna napisac " 'Author' "
 	isbn = models.CharField(max_length=17)
 	publisher = models.ForeignKey(Publisher)
+	def __str__(self):
+		return self.title
+
+#metoda wyswietlajaca reprezentacje napisową danego obiektu
